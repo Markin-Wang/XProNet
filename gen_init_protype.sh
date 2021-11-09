1a595ccbf27bc36dd79954ace5480d6098f9df14
Markin-Wang <cserwj@gmail.com>
2021-11-09 20:07:25 +0000

    add code to init memory

---
+++ gen_init_protype.sh
@@ -0,0 +1,25 @@
+python gen_init_protype.py \
+    --image_dir data/iu_xray/images/ \
+    --ann_path data/iu_xray/annotation.json \
+    --dataset_name iu_xray \
+    --max_seq_length 60 \
+    --threshold 3 \
+    --epochs 30 \
+    --batch_size 16 \
+    --lr_ve 5e-4 \
+    --lr_ed 1e-3 \
+    --step_size 10 \
+    --gamma 0.8 \
+    --num_layers 3 \
+    --topk 10 \
+    --cmm_size 2048 \
+    --cmm_dim 512 \
+    --seed 7580 \
+    --beam_size 3 \
+    --save_dir results/iu_xray/ \
+    --log_period 50 \
+    --n_gpu 1 \
+    --weight_cnn_loss 0.5 \
+    --num_cluster 15 \
+    --num_prototype 10 \
+
--- run_mimic_cxr.sh
+++ run_mimic_cxr.sh
@@ -5,17 +5,21 @@ python main.py \
     --max_seq_length 100 \
     --threshold 10 \
     --epochs 30 \
-    --batch_size 32 \
+    --batch_size 12 \
     --lr_ve 1e-4 \
     --lr_ed 5e-4 \
     --step_size 3 \
     --gamma 0.8 \
     --num_layers 3 \
-    --topk 32 \
+    --topk 10 \
     --cmm_size 2048 \
     --cmm_dim 512 \
     --seed 9153 \
     --beam_size 3 \
     --save_dir results/mimic_cxr/ \
     --log_period 1000 \
-    --n_gpu 4
+   --n_gpu 1 \
+    --weight_cnn_loss 0.5 \
+    --num_cluster 15 \
+    --num_prototype 10 \
+
