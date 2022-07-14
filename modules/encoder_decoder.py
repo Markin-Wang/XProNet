775d777688095edcbeed8e316d415e4255bf05f6
Jun Wang <45941861+Markin-Wang@users.noreply.github.com>
2022-07-14 12:39:30 +0100

    Update encoder_decoder.py

--- modules/encoder_decoder.py
+++ modules/encoder_decoder.py
@@ -462,15 +462,3 @@ class EncoderDecoder(AttModel):
 
         return out[:, -1], [ys.unsqueeze(0)] + past
     
-def con_loss(features, labels):
-    B, _ = features.shape
-    features = F.normalize(features)
-    cos_matrix = features.mm(features.t())
-    pos_label_matrix = torch.stack([labels == labels[i] for i in range(B)]).float()
-    neg_label_matrix = 1 - pos_label_matrix
-    pos_cos_matrix = 1 - cos_matrix
-    neg_cos_matrix = cos_matrix - 0.4
-    neg_cos_matrix[neg_cos_matrix < 0] = 0
-    loss = (pos_cos_matrix * pos_label_matrix).sum() + (neg_cos_matrix * neg_label_matrix).sum()
-    loss /= (B * B)
-    return loss
